f30be49a00872cacf65449a6f6bf07f29e8b91cf
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-05-08 20:12:13 -0700

    build: .nojekyll file

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -5,20 +5,10 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        // hydrate the <div id="svelte"> element in src/app.html
-        // target: "#svelte",
-        adapter: adapter({
-            // default options are shown
-            pages: "build",
-            assets: "build",
-            fallback: null,
-        }),
-        /*
-        If you need to serve your file from a sub directory
+        adapter: adapter(),
         paths: {
-            base: '/your-sub-dir',
+            base: "/test-twitter-dapp",
         },
-        */
     },
     preprocess: [
         preprocess({
